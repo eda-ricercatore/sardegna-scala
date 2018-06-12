56195939bb31d81f1a18b49217d429608131c0fb
Zhiyang Ong <ongz@acm.org>
2018-06-12 17:20:40 -0500

    Regression testing can begin now.

--- sandbox/test/Tester.scala
+++ sandbox/test/Tester.scala
@@ -1,11 +1,85 @@
 /**
+ * boilerplate-code: A library of common utility classes needed to
+ *	provide access to standard output/error stream, and file
+ *	input/output (I/O) operations.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright	(C)	<2014>	<Zhiyang Ong>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ *	obtaining a copy of this software and associated documentation
+ *	files (the "Software"), to deal in the Software without
+ *	restriction, including without limitation the rights to use, copy,
+ *	modify, merge, publish, distribute, sublicense, and/or sell copies
+ *	of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ *	included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *	DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * @author Zhiyang Ong
+ *
+ * Test suite for boilerplate-code.
+ *
+ * Main function for test suite:
+ * This test suite will test the various packages/modules, and
+ *	classes (and their functions) of boilerplate-code.
+ *
+ * For each class, the testing sequence would be:
+ * # constructors
+ * # accessor functions
+ * # mutator functions
+ * # destructor
+ *
+ * The current testing sequence of classes is:
+ * # utilities
+ *		@ violated_assertion
+ *		@ violated_postcondition
+ *		@ violated_precondition
+ *		@ printer
+ * ...
+ * # node
+ *
+ *
+ *
+ * #### IMPORTANT NOTES:
+ * To be completed...
+ *
+ *
+ *
+ *
+ *
+ *
  * References:
  *	http://www.scala-lang.org/documentation/getting-started.html
  *	http://www.scala-lang.org/
  */
 
-    object Tester {
-      def main(args: Array[String]): Unit = {
-        println("Hello, world!")
-      }
-    }
+
+object Tester {
+	// Start of main function...
+	def main(args: Array[String]): Unit = {
+		println("=============================================================")
+		println("		- START OF TEST SUITE - ")
+		println("=============================================================")
+		println("Run the automated regression test suite...")
+		println("")
+		println("")
+		println("")
+		println("")
+		println("=============================================================")
+		println("		- END OF TEST SUITE - ")
+		println("=============================================================")
+	}
+}
